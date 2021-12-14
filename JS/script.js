5f700099fa298b7dd57a6fa586e5ec8a5e92b6bd
Ivan <moonydis@gmail.com>
2021-12-14 15:30:43 +0300

    Second practice (if for)

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,36 @@
-const num = 50;
+const numberOfFilms = prompt('How many films do you watch?', '');
 
-switch (num) {
-    case 49:
-        console.log("Wrong");
-        break;
-    case 51:
-        console.log("Wrong");
-        break;
-    case 50:
-        console.log('Right');
-        break;
-    default:
-        console.log("not this time");
-        break;
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+for (i = 0; i < 2; i++) {
+    const a = prompt('One of saw film in last?');
+    b = prompt('What is your mark?');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Well done');
+    } else {
+        console.log('Error');
+        i--;
+    }
 }
+if (personalMovieDB.count < 10) {
+    console.log('To little show')
+    }
+    else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('You are usual user')
+    }
+    else if (personalMovieDB.count > 30) {
+        console.log('You are monster!')
+    }
+    else {
+        console.log('Oyoyoyoyoy')
+    }
+
+
+console.log(personalMovieDB);
